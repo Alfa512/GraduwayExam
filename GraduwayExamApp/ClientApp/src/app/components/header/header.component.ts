0f7f766fb2d0680986f0fa328e2503a20142da92
Muslimov Ruslan <alfa512ks@mail.ru>
2019-01-20 19:53:37 +0300

    Removed redundant files. Required links added to header.component

--- GraduwayExamApp/ClientApp/src/app/components/header/header.component.ts
+++ GraduwayExamApp/ClientApp/src/app/components/header/header.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { Helpers } from '@app/helpers/helpers';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+
+  authentication: boolean;
+  constructor(private helper: Helpers) {
+    this.authentication = helper.isAuthenticated();
+  }
+
+}
