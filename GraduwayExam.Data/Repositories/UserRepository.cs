5e685f98c44020eba88ffebfa6d92f3376ee5a24
Muslimov Ruslan <alfa512ks@mail.ru>
2019-01-22 17:42:46 +0300

    API: IDataContext link modified

--- GraduwayExam.Data/Repositories/UserRepository.cs
+++ GraduwayExam.Data/Repositories/UserRepository.cs
@@ -1,12 +1,15 @@
-﻿using GraduwayExam.Common.Contracts.Repositories;
+﻿using GraduwayExam.Common.Contracts.Data;
+using GraduwayExam.Common.Contracts.Repositories;
 using GraduwayExam.Data.Models;
 
 namespace GraduwayExam.Data.Repositories
 {
     public class UserRepository : GenericRepository<User>, IUserRepository
     {
-        public UserRepository(ApplicationContext context) : base(context)
+        private IDataContext _context;
+        public UserRepository(IDataContext context) : base(context)
         {
+            _context = context;
         }
     }
 }
